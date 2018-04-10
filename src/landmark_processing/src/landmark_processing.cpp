2dedcbb0e1249548b9887e548d4873919a13f8f7
Adam Driscoll <jdriscol@andrew.cmu.edu>
2018-04-10 11:23:22 -0400

    landmark parsing mostly done. need to figure out timestamp shenanigans and test

--- src/landmark_processing/src/landmark_processing.cpp
+++ src/landmark_processing/src/landmark_processing.cpp
@@ -5,17 +5,32 @@
 #include <tf/transform_broadcaster.h>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/Quaternion.h>
-
+#include <iostream>
+#include <fstream>
 
 ros::Subscriber sub;
 ImageConverter *ic;
 
 void apriltag_detection_callback(const apriltags_ros::AprilTagDetectionArray msg)
 {
- //TODO: Transform measurement to be from Grudsby Base rather than Grudsby Camera
  //TODO: Figure out timestep rate
  //TODO: Round apriltag timestamp to closest timstep
- //TODO: Format and dump out landmark info 
+  
+  //get the data from the message
+  unsigned int landmark_id = msg.detections[0].id;
+  double delta_x = msg.detection[0].pose.pose.position.x + 0.93209511; //hardcode the offset from camera to base because easier
+  double delta_y = msg.detection[0].pose.pose.position.y;
+  ros::Time timestamp_sec = msg.detection[0].pose.header.stamp.sec; //TODO: stamp.sec?? stamp.nsec??
+  ros::Time timestamp_nano = msg.detection[0].pose.header.stamp.nsec; 
+
+  //get timestamp into correct format 
+
+
+  //Dump to csv
+  std::ofstream out("landmarks.csv", std::ios::app);
+  out << timestamp << "," <<  landmark_i d<< "," << x << "," << y  << std::endl;
+  out.close();
+
 }
 
 int main(int argc, char** argv)
