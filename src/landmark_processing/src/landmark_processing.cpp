504174ed564256200ed760819b13a1940782041c
Adam Driscoll <jdriscol@andrew.cmu.edu>
2018-04-10 22:37:56 -0400

    both parsing nodes done and successfully building. Need to test

--- src/landmark_processing/src/landmark_processing.cpp
+++ src/landmark_processing/src/landmark_processing.cpp
@@ -1,56 +1,60 @@
 #include <ros/ros.h>
-#include <motion_decoder/image_converter.hpp>
 #include <apriltags_ros/AprilTagDetectionArray.h>
 #include <apriltags_ros/AprilTagDetection.h>
-#include <tf/transform_broadcaster.h>
-#include <geometry_msgs/Point.h>
-#include <geometry_msgs/Quaternion.h>
 #include <iostream>
 #include <fstream>
+#include <ros/console.h>
+#include <std_msgs/String.h>
 
-ros::Subscriber sub;
-//ImageConverter *ic;
+ros::Subscriber land_sub;
+ros::Subscriber state_sub;
+std::string state;
 
 void apriltag_detection_callback(const apriltags_ros::AprilTagDetectionArray msg)
 {
  //TODO: Figure out timestep rate
  //TODO: Round apriltag timestamp to closest timstep
   
-  //get the data from the message
-  unsigned int landmark_id = msg.detections[0].id;
 
-  //apriltags says z is forward, but we like it better when x is forward...
-  double delta_x = msg.detection[0].pose.pose.position.z + 0.93209511; //hardcode the offset from camera to base because easier
-  double delta_y = msg.detection[0].pose.pose.position.x;
-  double delta_z = 0;
-
-  std::cout << "Tag position: " << delta_x << ", " << delta_y << ", " << delta_z << std::endl;
-
-  ros::Time timestamp_sec = msg.detection[0].pose.header.stamp.sec; //TODO: stamp.sec?? stamp.nsec??
-  ros::Time timestamp_nano = msg.detection[0].pose.header.stamp.nsec;
-
-  std::cout << "Time: " << timestamp_sec << "\tnano: " << timestamp_nano << endl;
-
-  //get timestamp into correct format 
-
-  /* comment this out for now for testing porpoises
-  //Dump to csv
   std::ofstream out("landmarks.csv", std::ios::app);
-  out << timestamp << "," <<  landmark_i d<< "," << x << "," << y  << "," << z << std::endl;
+  for(auto &tag : msg.detections)
+  {  
+    //get the data from the message
+    int landmark_id = tag.id;
+
+    //apriltags says z is forward, but we like it better when x is forward
+    double delta_x = tag.pose.pose.position.z;// + 0.93209511; //hardcode the offset from camera to base because easier
+    double delta_y = tag.pose.pose.position.x;
+    double delta_z = 0;
+    //std::cout << "Tag: " << landmark_id << ", " << delta_x << ", " << delta_y << ", " << delta_z << std::endl;
+
+    //double timestamp_sec = msg.detections[0].pose.header.stamp.sec; //TODO: stamp.sec?? stamp.nsec??
+    //double timestamp_nano = msg.detections[0].pose.header.stamp.nsec;
+    //std::cout << "Time: " << timestamp_sec << "\tnano: " << timestamp_nano << std::endl;
+
+    //get timestamp into correct format 
+
+    //Dump to csv
+    out << state << "," <<  landmark_id << "," << delta_x << "," << delta_y  << "," << delta_z << std::endl;
+  }
   out.close();
-  */
+}
+
+void state_callback(const std_msgs::String msg)
+{
+  state = msg.data;
 }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "image_converter");
+  ros::init(argc, argv, "landmark_processing");
 
   ros::NodeHandle n;
 
- // ImageCoverter converter;
+  land_sub = n.subscribe("tag_detections", 1000, apriltag_detection_callback);
+  state_sub = n.subscribe("state_counter", 1000, state_callback);
 
-  ic = &converter;
-  sub = n.subscribe("tag_detections", 1000, apriltag_detection_callback);
+  //std::cout.precision(10);
 
   ros::Rate loop_rate(50);
   
@@ -59,4 +63,6 @@ int main(int argc, char** argv)
     ros::spinOnce();
     loop_rate.sleep();
   }
+  return 0;
 }
+
---
+++ src/odom_processing/src/odom_processing.cpp
@@ -0,0 +1,55 @@
+#include <ros/ros.h>
+#include <iostream>
+#include <fstream>
+#include <ros/console.h>
+#include <std_msgs/String.h>
+#include <nav_msgs/Odometry.h>
+#include <sstream>
+
+ros::Subscriber sub;
+ros::Publisher pub;
+std_msgs::String state_string;
+unsigned int state;
+
+void odom_callback(const nav_msgs::Odometry msg)
+{
+  //TODO: this entire thing
+  ++state;
+  std::stringstream ss;
+  ss << state;
+  state_string.data = ss.str();
+  pub.publish(state_string);
+
+  double vx = msg.twist.twist.linear.x;
+  double l = 0.508;
+  double omega = msg.twist.twist.angular.z;
+
+  double Vl = vx - ( omega * (l/2) );
+  double Vr = vx + ( omega * (l/2) );
+
+  std::ofstream out("odom.csv", std::ios::app);
+  out << state << "," << Vl << "," << Vr << std::endl;
+  out.close();
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "odom_processing");
+
+  ros::NodeHandle n;
+
+  sub = n.subscribe("", 1000, odom_callback);
+  pub = n.advertise<std_msgs::String>("state_counter", 1000);
+
+  state = 0;
+
+  ros::Rate loop_rate(50);
+
+  while(ros::ok())
+  {
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+  return 0;
+} 
