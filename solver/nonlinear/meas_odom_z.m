7879255a7cc700ddac4c82a0298b7f813cbb2c0c
jphaneuf <phaneuf.joe>
2018-04-28 22:16:06 -0400

    fix scaling / dt issue

--- solver/nonlinear/meas_odom_z.m
+++ solver/nonlinear/meas_odom_z.m
@@ -1,12 +1,7 @@
 function [z] = meas_odom_z(vl, vr, rt)
     L = .508;
     z = zeros(3, 1);
-    dt = 1/10.0;
-    %%%%%%%% This is a grudsby related bamboozle
-    scale = 2;
-    vr = vr / scale;
-    vl = vl / scale;
-    %%%/ bamboozle %%%%%
+    dt = 1/20.0;
 
     z(1) = dt * ( (vr + vl)/2 ) * cos(rt);
     z(2) = dt * ( (vr + vl)/2 ) * sin(rt);
