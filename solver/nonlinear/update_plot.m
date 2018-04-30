3dbb035d5b7c9f28b98c34cc8432e8137e3cd1ee
Adam Driscoll <jdriscol@andrew.cmu.edu>
2018-04-29 23:51:54 -0400

    update_plot doesn't suck now

--- solver/nonlinear/update_plot.m
+++ solver/nonlinear/update_plot.m
@@ -8,13 +8,15 @@ function update_plot(name, est_traj, est_landmarks, odom, gt_traj, gt_landmarks)
 figure(1); clf; hold on;
 %plot(gt_traj(:,1), gt_traj(:,2), 'k')
 odom_only = [0 0; cumsum(odom)];
-plot(odom_only(:,1), odom_only(:,2), 'r')
+% plot(odom_only(:,1), odom_only(:,2), 'r')
 plot(est_traj(1:end,1), est_traj(1:end,2), 'b')
 
 %plot(gt_landmarks(:,1), gt_landmarks(:,2), 'ko')
 plot(est_landmarks(:,1), est_landmarks(:,2), 'bx')
 
-legend('ground truth', 'pure odometry', 'full SLAM', 'Location', 'best');
+% legend('ground truth', 'pure odometry', 'full SLAM', 'Location', 'best');
+legend('full SLAM', 'Landmarks', 'Location', 'best');
 title(sprintf('Results for %s', name));
-xlim([0 10]);
-ylim([0, 4.5]);
+daspect([1 1 1])
+% xlim([0 10]);
+% ylim([0, 4.5]);
